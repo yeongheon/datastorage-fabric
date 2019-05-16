af0ae082fd44d0880f64504dd2f7775c67fc283b
송영헌 <yeongheon@iPotato.local>
2019-05-16 20:03:05 +0900

    Complete draft of network.js

--- server/src/fabric/network.js
+++ server/src/fabric/network.js
@@ -21,6 +21,7 @@ const ccp = JSON.parse(ccpJSON)
 exports.createAsset = async function(assetID, assetURI, assetHash) {
     try {
         let response = {}
+        let timestamp = new Date().toISOString()
 
         // make new wallet based on filesystem
         const walletPath = path.join(process.cwd(), '/wallet')
@@ -51,7 +52,7 @@ exports.createAsset = async function(assetID, assetURI, assetHash) {
         const contract = network.getContract('datastorage')
 
         // submit tx
-        await contract.submitTransaction('createAsset', assetID, userName, assetURI, assetHash)
+        await contract.submitTransaction('createAsset', assetID, userName, assetURI, assetHash, timestamp)
         console.log('Transaction has been submitted')
 
         // disconnect from the gateway
@@ -68,26 +69,301 @@ exports.createAsset = async function(assetID, assetURI, assetHash) {
     }
 } // Create Research Asset
 
-exports.updateAsset = async function(assetID, researcherID, assetURI, assetHash) {
-    // func
+exports.updateAsset = async function(assetID, userName, assetURI, assetHash) {
+exports.queryAllAsset = async function() {
+    try {
+        let response = {}
+
+        // make new wallet based on filesystem
+        const walletPath = path.join(process.cwd(), '/wallet')
+        const wallet = new FileSystemWallet(walletPath)
+        console.log(`Wallet Path: ${walletPath}`)
+
+        const userExists = await wallet.userExists(userName)
+        if (!userExists) {
+            console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+            console.log('Run the registerUser.js before run this script')
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+
+            return response
+        }
+
+        // create new gateway to connect peer
+        const gateway = new Gateway()
+        await gateway.connect(ccp, {
+            wallet: wallet,
+            identity: userName,
+            discovery: gatewayDiscovery
+        })
+
+        // get the network to deploy contract
+        const network = await gateway.getNetwork('mychannel')
+        
+        // get the contract from the network
+        const contract = network.getContract('datastorage')
+
+        // submit tx
+        const result = await contract.evaluateTransaction('queryAllAsset')
+        console.log(`Transaction has been evaluated, result is ${result.toString}`)
+
+        // disconnect from the gateway
+        await gateway.disconnect()
+
+        return result
+    } catch (e) {
+        console.error(`Failed to evaluate transaction: ${e}`)
+        let response = {}
+        response.error = e.message
+        
+        return response
+    }
 } // Update Research Asset to New Version
 
 exports.queryAllAsset = async function() {
-    // func
+    try {
+        let response = {}
+
+        // make new wallet based on filesystem
+        const walletPath = path.join(process.cwd(), '/wallet')
+        const wallet = new FileSystemWallet(walletPath)
+        console.log(`Wallet Path: ${walletPath}`)
+
+        const userExists = await wallet.userExists(userName)
+        if (!userExists) {
+            console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+            console.log('Run the registerUser.js before run this script')
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+
+            return response
+        }
+
+        // create new gateway to connect peer
+        const gateway = new Gateway()
+        await gateway.connect(ccp, {
+            wallet: wallet,
+            identity: userName,
+            discovery: gatewayDiscovery
+        })
+
+        // get the network to deploy contract
+        const network = await gateway.getNetwork('mychannel')
+        
+        // get the contract from the network
+        const contract = network.getContract('datastorage')
+
+        // submit tx
+        const result = await contract.evaluateTransaction('queryAllAsset')
+        console.log(`Transaction has been evaluated, result is ${result.toString}`)
+
+        // disconnect from the gateway
+        await gateway.disconnect()
+
+        return result
+    } catch (e) {
+        console.error(`Failed to evaluate transaction: ${e}`)
+        let response = {}
+        response.error = e.message
+        
+        return response
+    }
 } // Query all Asset without URI information
 
 exports.queryAssetbyID = async function(assetID) {
-    // func
+    exports.queryAllAsset = async function() {
+        try {
+            let response = {}
+    
+            // make new wallet based on filesystem
+            const walletPath = path.join(process.cwd(), '/wallet')
+            const wallet = new FileSystemWallet(walletPath)
+            console.log(`Wallet Path: ${walletPath}`)
+    
+            const userExists = await wallet.userExists(userName)
+            if (!userExists) {
+                console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+                console.log('Run the registerUser.js before run this script')
+                response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+    
+                return response
+            }
+    
+            // create new gateway to connect peer
+            const gateway = new Gateway()
+            await gateway.connect(ccp, {
+                wallet: wallet,
+                identity: userName,
+                discovery: gatewayDiscovery
+            })
+    
+            // get the network to deploy contract
+            const network = await gateway.getNetwork('mychannel')
+            
+            // get the contract from the network
+            const contract = network.getContract('datastorage')
+    
+            // submit tx
+            const result = await contract.evaluateTransaction('queryAssetbyID', assetID, userName)
+            console.log(`Transaction has been evaluated, result is ${result.toString}`)
+    
+            // disconnect from the gateway
+            await gateway.disconnect()
+    
+            return result
+        } catch (e) {
+            console.error(`Failed to evaluate transaction: ${e}`)
+            let response = {}
+            response.error = e.message
+            
+            return response
+        }
 } // Query target asset by ID
 
-exports.requestAccess = async function (assetID, researcherID) {
-    // func
+exports.requestAccess = async function (assetID, userName) {
+    try {
+        let response = {}
+
+        // make new wallet based on filesystem
+        const walletPath = path.join(process.cwd(), '/wallet')
+        const wallet = new FileSystemWallet(walletPath)
+        console.log(`Wallet Path: ${walletPath}`)
+
+        const userExists = await wallet.userExists(userName)
+        if (!userExists) {
+            console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+            console.log('Run the registerUser.js before run this script')
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+
+            return response
+        }
+
+        // create new gateway to connect peer
+        const gateway = new Gateway()
+        await gateway.connect(ccp, {
+            wallet: wallet,
+            identity: userName,
+            discovery: gatewayDiscovery
+        })
+
+        // get the network to deploy contract
+        const network = await gateway.getNetwork('mychannel')
+        
+        // get the contract from the network
+        const contract = network.getContract('datastorage')
+
+        // submit tx
+        await contract.submitTransaction('requestAccess', assetID, userName)
+        console.log('Transaction has been submitted')
+
+        // disconnect from the gateway
+        await gateway.disconnect()
+
+        response.msg = 'requestAccess Transaction has been submitted'
+        return response
+    } catch (e) {
+        console.error(`Failed to submit transaction: ${e}`)
+        let response = {}
+        response.error = e.message
+        
+        return response
+    }
 } // Request permission of research data URI
 
 exports.grantAccess = async function (assetID, researcherID) {
-    // func
+    try {
+        let response = {}
+
+        // make new wallet based on filesystem
+        const walletPath = path.join(process.cwd(), '/wallet')
+        const wallet = new FileSystemWallet(walletPath)
+        console.log(`Wallet Path: ${walletPath}`)
+
+        const userExists = await wallet.userExists(userName)
+        if (!userExists) {
+            console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+            console.log('Run the registerUser.js before run this script')
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+
+            return response
+        }
+
+        // create new gateway to connect peer
+        const gateway = new Gateway()
+        await gateway.connect(ccp, {
+            wallet: wallet,
+            identity: userName,
+            discovery: gatewayDiscovery
+        })
+
+        // get the network to deploy contract
+        const network = await gateway.getNetwork('mychannel')
+        
+        // get the contract from the network
+        const contract = network.getContract('datastorage')
+
+        // submit tx
+        await contract.submitTransaction('grantAccess', assetID, userName, researcherID)
+        console.log('Transaction has been submitted')
+
+        // disconnect from the gateway
+        await gateway.disconnect()
+
+        response.msg = 'grantAccess Transaction has been submitted'
+        return response
+    } catch (e) {
+        console.error(`Failed to submit transaction: ${e}`)
+        let response = {}
+        response.error = e.message
+        
+        return response
+    }
 } // Grant permission of research data URI
 
 exports.revokeAccess = async function (assetID, researcherID) {
-    // func
+    try {
+        let response = {}
+
+        // make new wallet based on filesystem
+        const walletPath = path.join(process.cwd(), '/wallet')
+        const wallet = new FileSystemWallet(walletPath)
+        console.log(`Wallet Path: ${walletPath}`)
+
+        const userExists = await wallet.userExists(userName)
+        if (!userExists) {
+            console.log('An identity for User ' + userName + ' doesn not exists in the Wallet')
+            console.log('Run the registerUser.js before run this script')
+            response.error = 'An identity for the user ' + userName + ' does not exist in the wallet. Register ' + userName + ' first'
+
+            return response
+        }
+
+        // create new gateway to connect peer
+        const gateway = new Gateway()
+        await gateway.connect(ccp, {
+            wallet: wallet,
+            identity: userName,
+            discovery: gatewayDiscovery
+        })
+
+        // get the network to deploy contract
+        const network = await gateway.getNetwork('mychannel')
+        
+        // get the contract from the network
+        const contract = network.getContract('datastorage')
+
+        // submit tx
+        await contract.submitTransaction('revokeAccess', assetID, userName, researcherID)
+        console.log('Transaction has been submitted')
+
+        // disconnect from the gateway
+        await gateway.disconnect()
+
+        response.msg = 'revokeAccess Transaction has been submitted'
+        return response
+    } catch (e) {
+        console.error(`Failed to submit transaction: ${e}`)
+        let response = {}
+        response.error = e.message
+        
+        return response
+    }
 } // Revoke permission of research data URI
